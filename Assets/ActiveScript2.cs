2757e39f23f4fdc89d900a95d71cf45489fceb4e
Jerax1337 <anton.shap_2002@mail.ru>
2023-10-21 22:25:49 +0300

    Test
    Test

---
+++ Assets/ActiveScript2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveScript2 : MonoBehaviour
+{
+    public GameObject sphere;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
